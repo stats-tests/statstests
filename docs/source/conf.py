b389aadf6934479bbb6b2b6da6bfa3571fe175d0
helderprado <helderprado@gmail.com>
2022-09-19 18:37:07 -0300

    new page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,5 +26,31 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_material'
 html_static_path = ['_static']
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Statstests',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://project.github.io/project',
+
+    # Set the color and the accent color
+    'color_primary': 'deep-purple',
+    'color_accent': 'deep-orange',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://stats-tests.github.io/statstests/',
+    'repo_name': 'statstests',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
--- statstests/process.py
+++ statstests/process.py
@@ -2,11 +2,92 @@ import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
-def stepwise(model, pvalue_limit=0.05):
+
+def stepwise(model, pvalue_limit: float=0.05):
     """
 
     Stepwise process for GLM models
 
+    >>> import pandas as pd
+    >>> import statsmodels.api as sm
+    >>> from statstests.process import stepwise
+    >>> model = sm.OLS.from_formula('retorno ~ disclosure + endividamento + ativos + liquidez', df).fit()
+    >>> print(model.summary())
+                                OLS Regression Results                            
+    ==============================================================================
+    Dep. Variable:                retorno   R-squared:                       0.833
+    Model:                            OLS   Adj. R-squared:                  0.827
+    Method:                 Least Squares   F-statistic:                     147.9
+    Date:                Mon, 19 Sep 2022   Prob (F-statistic):           3.35e-45
+    Time:                        21:26:20   Log-Likelihood:                -401.07
+    No. Observations:                 124   AIC:                             812.1
+    Df Residuals:                     119   BIC:                             826.2
+    Df Model:                           4                                         
+    Covariance Type:            nonrobust                                         
+    =================================================================================
+                        coef    std err          t      P>|t|      [0.025      0.975]
+    ---------------------------------------------------------------------------------
+    Intercept         6.0506      4.080      1.483      0.141      -2.028      14.129
+    disclosure        0.1067      0.048      2.227      0.028       0.012       0.202
+    endividamento    -0.0882      0.051     -1.723      0.087      -0.190       0.013
+    ativos            0.0035      0.001      5.134      0.000       0.002       0.005
+    liquidez          1.9762      0.396      4.987      0.000       1.191       2.761
+    ==============================================================================
+    Omnibus:                       35.509   Durbin-Watson:                   2.065
+    Prob(Omnibus):                  0.000   Jarque-Bera (JB):                7.127
+    Skew:                          -0.136   Prob(JB):                       0.0283
+    Kurtosis:                       1.858   Cond. No.                     2.94e+04
+    ==============================================================================
+    Notes:
+    [1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+    [2] The condition number is large, 2.94e+04. This might indicate that there are
+    strong multicollinearity or other numerical problems.
+
+    Examples
+
+    >>> stepwise(modelo_empresas)
+    Regression type: OLS 
+    Estimating model...: 
+    retorno ~ disclosure + endividamento + ativos + liquidez
+    Discarding atribute "endividamento" with p-value equal to 0.08749071283026402 
+    Estimating model...: 
+    retorno ~ disclosure + ativos + liquidez
+    Discarding atribute "disclosure" with p-value equal to 0.06514029954310786 
+    Estimating model...: 
+    retorno ~ ativos + liquidez
+    No more atributes with p-value higher than 0.05
+    Atributes discarded on the process...: 
+    {'atribute': 'endividamento', 'p-value': 0.08749071283026402}
+    {'atribute': 'disclosure', 'p-value': 0.06514029954310786}
+    Model after stepwise process...: 
+    retorno ~ ativos + liquidez 
+                                OLS Regression Results                            
+    ==============================================================================
+    Dep. Variable:                retorno   R-squared:                       0.823
+    Model:                            OLS   Adj. R-squared:                  0.820
+    Method:                 Least Squares   F-statistic:                     282.1
+    Date:                Mon, 19 Sep 2022   Prob (F-statistic):           2.76e-46
+    Time:                        21:29:20   Log-Likelihood:                -404.37
+    No. Observations:                 124   AIC:                             814.7
+    Df Residuals:                     121   BIC:                             823.2
+    Df Model:                           2                                         
+    Covariance Type:            nonrobust                                         
+    ==============================================================================
+                    coef    std err          t      P>|t|      [0.025      0.975]
+    ------------------------------------------------------------------------------
+    Intercept     -2.5348      2.341     -1.083      0.281      -7.169       2.100
+    ativos         0.0040      0.001      7.649      0.000       0.003       0.005
+    liquidez       2.7391      0.258     10.637      0.000       2.229       3.249
+    ==============================================================================
+    Omnibus:                       23.591   Durbin-Watson:                   1.926
+    Prob(Omnibus):                  0.000   Jarque-Bera (JB):                5.887
+    Skew:                          -0.087   Prob(JB):                       0.0527
+    Kurtosis:                       1.947   Cond. No.                     1.65e+04
+    ==============================================================================
+    Notes:
+    [1] Standard Errors assume that the covariance matrix of the errors is correctly specified.
+    [2] The condition number is large, 1.65e+04. This might indicate that there are
+    strong multicollinearity or other numerical problems.
     """
 
     # dictionary that identifies the type of the inputed model
