78c1c04fd8e905d682ac1c485e86c032b4cbb63b
helderprado <helderprado@gmail.com>
2022-09-20 12:03:44 -0300

    definindo base url

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'statstests')))
+sys.path.insert(0, os.path.abspath(os.path.join('../../statstests')))
 
 #
 # For the full list of built-in configuration values, see the documentation:
